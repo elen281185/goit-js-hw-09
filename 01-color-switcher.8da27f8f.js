343087b2995cbacafb69ee7db11f7ba28ea74c97
elen281185 <119810075+elen281185@users.noreply.github.com>
2023-10-09 18:58:09 +0000

    Deploying to gh-pages from @ elen281185/goit-js-hw-09@0eec0b637a7e1b7bc3c15d95d9be5f82f56413bc 🚀

--- 01-color-switcher.8da27f8f.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(a),t.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.8da27f8f.js.map
